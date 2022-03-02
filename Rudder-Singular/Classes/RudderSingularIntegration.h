3e2c939e76de9f561645b173753e3e2cf872d708
1abhishekpandey <abhishekpandey10196@gmail.com>
2022-03-02 18:58:06 +0530

    Implemented Singular device mode integration

--- Rudder-Singular/Classes/RudderSingularIntegration.h
+++ Rudder-Singular/Classes/RudderSingularIntegration.h
@@ -16,6 +16,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 - (instancetype)initWithConfig:(NSDictionary *)config withAnalytics:(RSClient *)client;
++ (void)setSKANOptions:(BOOL)skAdNetworkEnabled isManualSkanConversionManagementMode:(BOOL)manualMode withWaitForTrackingAuthorizationWithTimeoutInterval:(NSNumber* _Nullable)waitTrackingAuthorizationWithTimeoutInterval withConversionValueUpdatedHandler:(void(^_Nullable)(NSInteger))conversionValueUpdatedHandler;
 
 @end
 
